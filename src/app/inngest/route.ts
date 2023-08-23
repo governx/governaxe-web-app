97c92ff00c4cfb9968ddaa3cbb3911a19411a9f9
freeed <liberatheon@gmail.com>
2023-08-23 22:27:57 +0700

    change event name

--- src/app/inngest/route.ts
+++ src/app/inngest/route.ts
@@ -1,4 +1,4 @@
 import { serve } from "inngest/next";
-import { queueProposal, inngest } from "@/inngest";
+import { executeProposal, inngest } from "@/inngest";
 
-export const { GET, POST, PUT } = serve(inngest, [queueProposal]);
+export const { GET, POST, PUT } = serve(inngest, [executeProposal]);
