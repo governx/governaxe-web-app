b7f5eb2b34d472ea1f53d01236b6ee02e89d04f7
ayvee <asianvikingofficial@gmail.com>
2023-09-20 13:38:04 +0700

    ✨ add favicon / openimage / metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,54 @@
 import { NavBar } from "@/components/main-nav";
 import "../../styles/globals.css";
 import type { Metadata } from "next";
+import { siteConfig } from "@/config/site";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import Web3Provider from "./Web3Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Governaxe",
-  description: "Omnichain Governance",
+export const metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Governance",
+    "Axelar",
+    "Hackathon",
+    "Crosschain",
+    "Multichain",
+    "Omnichain",
+    "Interchain",
+  ],
+  creator: "shadcn",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og.png`],
+    creator: "@shadcn",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Governaxe",
+  description:
+    "Governaxe securely carries out on-chain actions across multiple blockchains through the Axelar Network, enabling protocols to manage multi-chain assets and governance seamlessly.",
+
+  url: "https://governaxe.xyz",
+  ogImage: "/og.png",
+};
