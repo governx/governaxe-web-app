b8637918aa224dbcf2459b73bda75dbd5ff6a20b
ayvee <asianvikingofficial@gmail.com>
2023-09-20 11:28:17 +0700

    🔥 remove unused code & imports
    Remove unused code & imports in app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 "use client";
+
 import { Key, useEffect, useState } from "react";
+
 import { Input } from "@/components/ui/input";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import Link from "next/link";
-// import { getClient } from "@/lib/gqlClient";
 
 import { useAccount } from "wagmi";
-
-interface Spaces {}
 
 export default function Home() {
   const { address, isConnected } = useAccount();
@@ -17,7 +22,6 @@ export default function Home() {
 
   useEffect(() => {
     if (isConnected) {
-      // const datra
       const fetchData = async () => {
         const GET_FOLLOWS = `
           query getFollows($follower: String!) {
@@ -34,8 +38,6 @@ export default function Home() {
           }
         `;
 
-        console.log(GET_FOLLOWS);
-        // get the data from the api
         const url = "https://testnet.snapshot.org/graphql";
         const response = await fetch(url, {
           method: "POST",
@@ -50,12 +52,8 @@ export default function Home() {
           }),
         });
 
-        // // convert the data to json
         const json = await response.json();
 
-        console.log(json);
-
-        // set state with the result
         setData(json.data);
       };
 
@@ -68,16 +66,20 @@ export default function Home() {
 
   return (
     <main
-      className='flex flex-col items-center justify-between p-8'
+      className="flex flex-col items-center justify-between p-8"
       style={{
         minHeight: "calc(100vh - 5rem)",
       }}
     >
-      <div className='w-full max-w-6xl gap-4'>
-        <div className='flex gap-4'>
-          <Input className='lg:w-1/3 md:w-2/3' type='search' placeholder='Search' />
+      <div className="w-full max-w-6xl gap-4">
+        <div className="flex gap-4">
+          <Input
+            className="lg:w-1/3 md:w-2/3"
+            type="search"
+            placeholder="Search"
+          />
         </div>
-        <div className='gap-4 grid grid-cols-12 mt-4'>
+        <div className="gap-4 grid grid-cols-12 mt-4">
           {isConnected &&
             data &&
             // @ts-ignore
@@ -91,35 +93,35 @@ export default function Home() {
                     followersCount: number;
                   };
                 },
-                index: Key | null | undefined
+                index: Key | null | undefined,
               ) => (
                 <Link
                   href={`/${dao.space?.id}`}
                   key={index}
-                  className='lg:col-span-3 md:col-span-4 col-span-12'
+                  className="lg:col-span-3 md:col-span-4 col-span-12"
                 >
-                  <Card className='hover:border-zinc-50 cursor-pointer'>
+                  <Card className="hover:border-zinc-50 cursor-pointer">
                     <CardHeader>
-                      <Avatar className='mb-2'>
+                      <Avatar className="mb-2">
                         <AvatarImage
                           src={`https://cloudflare-ipfs.com/ipfs/${
                             dao.space.avatar.split("://")[1]
                           }`}
                           alt={dao.space?.name}
                         />
-                        <AvatarFallback>{dao.space?.name.slice(0, 2)}</AvatarFallback>
+                        <AvatarFallback>
+                          {dao.space?.name.slice(0, 2)}
+                        </AvatarFallback>
                       </Avatar>
                       <CardTitle>{dao.space?.name}</CardTitle>
-                      <CardDescription>{dao.space?.followersCount} Members</CardDescription>
+                      <CardDescription>
+                        {dao.space?.followersCount} Members
+                      </CardDescription>
                     </CardHeader>
-                    <CardFooter>
-                      {/* <Button className='w-full' variant='outline'>
-                      Join
-                    </Button> */}
-                    </CardFooter>
+                    <CardFooter></CardFooter>
                   </Card>
                 </Link>
-              )
+              ),
             )}
         </div>
       </div>
